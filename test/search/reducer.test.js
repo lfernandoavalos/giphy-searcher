9d5efe294dee9227ed7e9688b132e4713b598ad5
Fernando Avalos <lfernandoavalos@gmail.com>
2018-03-18 17:26:02 -0400

    Add redux redcuer testing

---
+++ test/search/reducer.test.js
@@ -0,0 +1,60 @@
+import expect from 'expect';
+
+import reducer, { initialState } from './../../src/Modules/Main/redux/search.reducer';
+import * as types from './../../src/Modules/Main/redux/search.types';
+
+import { TRENDING } from './__mock__';
+
+
+describe('search reducer', () => {
+  it('should return the initial state', () => {
+    const action = {
+      type: types.FETCH_GIFS_LOADING,
+    };
+    expect(reducer({}, action)).toEqual(initialState);
+    expect(reducer(false, action)).toEqual(initialState);
+    expect(reducer(null, action)).toEqual(initialState);
+  });
+
+  it('should handle FETCH_GIFS_SUCCESS', () => {
+    const action = {
+      type: types.FETCH_GIFS_SUCCESS,
+      payload: {
+        results: TRENDING,
+      },
+    };
+
+    expect(reducer(initialState, action)).toEqual({
+      ...initialState,
+      results: TRENDING,
+      offset: 25,
+      searchAsyncInProgress: false,
+      searchTerm: undefined,
+    });
+  });
+
+  it('should handle FETCH_GIFS_APPEND_SUCCESS', () => {
+    const state = reducer(initialState, {
+      type: types.FETCH_GIFS_SUCCESS,
+      payload: {
+        results: TRENDING,
+        searchTerm: 'Searching',
+      },
+    });
+
+    const action = {
+      type: types.FETCH_GIFS_APPEND_SUCCESS,
+      payload: {
+        results: [...TRENDING],
+      },
+    };
+
+    expect(reducer(state, action)).toEqual({
+      ...initialState,
+      results: [...TRENDING, ...TRENDING],
+      offset: 50,
+      searchAsyncInProgress: false,
+      searchTerm: 'Searching',
+    });
+  });
+});
